7f738cd975c677ea77ab71a0e109f6342061a4d8
arsal <mh07607@st.habib.edu.pk>
2023-06-12 20:21:18 +0500

    rudimentary homepage

--- App.js
+++ App.js
@@ -1,25 +1,74 @@
 import { React } from 'react';
-import { View, Text, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import { View, Text, StatusBar, StyleSheet, TextInput, Dimensions, TouchableOpacity } from 'react-native';
+// import { styles } from './styles/AppStyles'
 
 
 const App = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={style.container}>
-        <Text>Sign In!</Text>
+    <View style={styles.top}>
+      <View style={styles.home}>
+        <Text style={styles.homeText}>Home</Text>
       </View>
-    </SafeAreaView>
+      <TextInput style={styles.searchBar}
+        placeholder='Search'
+      />
+      <View style={styles.cards}>
+        <TouchableOpacity style={styles.card}>
+          <Text style={{fontSize: 24, color: 'gray'}}>Book a Ride</Text>  
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.card}>
+          <Text style={{fontSize: 24, color: 'gray'}}>Host a Ride</Text>  
+        </TouchableOpacity>
+      </View>
+      <View style = {styles.settings}>
+        <Text style={{fontSize: 16, color: 'gray'}}>Settings</Text>
+      </View>
+    </View>
   );
 }
 
-const style = StyleSheet.create({
-  container: {
-    backgroundColor: 'black',
+const styles = StyleSheet.create({
+  top:{
     marginTop: StatusBar.currentHeight,
-    flex: 2,
+    marginBottom: 25,
+    flexDirection: 'column',
+    gap: 10,
+    flex: 1
+  }
+  ,
+  home: {
+    backgroundColor: 'aquamarine',
     alignItems: 'center',
     justifyContent: 'center',
-    color: 'white'
+    flex: 1
+  },
+  homeText:{
+    fontSize: 35,
+    color: 'gray',
+  },
+  searchBar: {
+    backgroundColor: 'whitesmoke',
+    borderRadius: 20,
+    flex: 0.5,
+    alignSelf: 'center',
+    width: Dimensions.get('window').width-100,
+  },
+  cards: {
+    flex: 10,
+    gap: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    width: 300,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  settings: {
+    flex: 1, 
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
 
---
+++ styles/AppStyles.js
@@ -0,0 +1,23 @@
+import { StatusBar, StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    home: {
+      backgroundColor: 'aquamarine',
+      marginTop: StatusBar.currentHeight,
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: 'blue'
+    },
+    homeText:{
+      fontSize: 35,
+      color: 'white',
+    },
+    searchBar: {
+      
+    }
+  });
+
+
+export default styles;
+  
