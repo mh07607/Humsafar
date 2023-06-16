e7fa91a54a15dc945ac50ec4de18c9fdd8b2cb2a
mh07607 <mh07607@st.habib.edu.pk>
2023-06-16 11:04:53 +0500

    Login screen

--- App.js
+++ App.js
@@ -1,31 +1,25 @@
 import {React} from 'react';
 import Home  from './components/Home';
-import {View, Text, TouchableOpacity} from 'react-native';
+import Login  from './components/Login';
+// import {View, Text, TouchableOpacity} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
-function DetailsScreen({navigation}) {
-  return (
-    <View style={{backgroundColor: "#FAFAFC", flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      
-      <TouchableOpacity 
-            // style={styles.card}
-            onPressOut={()=>{
-              navigation.navigate('Home');
-    }}>
-        <Text>Go back to Home screen</Text>
-  </TouchableOpacity>
-    </View>
-  );
-}
-
 const App = () => {
   return (
   <NavigationContainer>
     <Stack.Navigator>
+      <Stack.Screen 
+        name="Login" 
+        component={Login}
+        options = {{
+          headerTitle: "Login",
+          headerShown: false,
+          headerShadowVisible: false,
+          headerStyle: {backgroundColor: "#FAFAFC"}
+         }} />
       <Stack.Screen name="Home"
        component={Home}
        options = {{
@@ -34,14 +28,6 @@ const App = () => {
         headerStyle: {backgroundColor: "#FAFAFC"}
        }}
        />
-      <Stack.Screen 
-        name="Details" 
-        component={DetailsScreen}
-        options = {{
-          headerTitle: "",
-          headerShadowVisible: false,
-          headerStyle: {backgroundColor: "#FAFAFC"}
-         }} />
 
     </Stack.Navigator>
   </NavigationContainer>  
---
+++ components/login.style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    loginBanner: {
+        backgroundColor: 'aquamarine',
+        position: 'absolute',
+        width: 606,
+        height: 606,
+        left: -123,
+        top: -206,
+
+        // background: url(.jpg);
+    }
+});
+
+export default styles;
