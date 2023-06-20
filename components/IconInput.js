b712650232cd64f420e8cb5bbb8d78e04818884f
mh07607 <mh07607@st.habib.edu.pk>
2023-06-20 12:08:13 +0500

    login screen done

--- components/IconInput.js
+++ components/IconInput.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import {TextInput, View, Image} from 'react-native';
-import styles from './iconinput.style';
+import styles from './iconinput.style'; 
 
-const IconInput = ({placeHolder, icon}) => {
+const IconInput = ({placeHolder, icon, hide}) => {
 
     return (
-        <View style={styles.iconBorder}>
-            <Image source={icon} />
-            <View>
-                <TextInput
-                placeholder={placeHolder}
-                style={styles.input}>
-                </TextInput>
-                <View style={styles.line}></View>
+        <View>
+            <View style={styles.iconBorder}>
+                <Image source={icon} />
+                <View>
+                    <TextInput secureTextEntry={hide}
+                    placeholder={placeHolder}
+                    style={styles.input}>
+                    </TextInput>
+                    <View style={styles.line}></View>
+                </View>
             </View>
         </View>
     );
